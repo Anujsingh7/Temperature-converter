c57fb7d60f54b061343fe23f8820ddf0d77398f7
anuj singh <anujsinghpp123@gmail.com>
2023-09-04 21:58:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const calculateTemp =()=>{
+    const numberTemp =document.getElementById('temp').value;
+
+    const tempSelected =document.getElementById('temp_diff');
+    const valueTemp=temp_diff.options[tempSelected.selectedIndex].value;
+    
+    const celToFah =(cel)=>{
+        return Math.round((cel*9/5)+32);
+       
+    }
+    const fahToCel =(fah)=>{
+        return Math.round((fah-32)*5/9);
+    }
+
+    let result;
+    if(valueTemp=='cel')
+    {
+        result=celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML=`= ${result} Fahrenheit`;
+    }
+    else{
+        result=fahToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML=`= ${result} Celsius`;
+
+    }
+
+}
